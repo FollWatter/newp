90a292474bcfff5385967844157f7d33e9bdabda
FollWatter <FollWatter@ukr.net>
2020-10-28 15:01:35 +0200

    add Площа поверхні куба

--- newpr.cpp
+++ newpr.cpp
@@ -12,8 +12,10 @@ int main()
     double S, Pi=3.14, V, r, h=4;
     printf("Введіть радіус:");
     scanf_s("%lf" ,& r);
+    S = 2 * Pi * r * (r+h);
     S = 2 * Pi * r * h;
     V = Pi * r * r * h;
+    printf("Площа поверхні куба =%if\n", S);
     printf("Площа бічної поверхні куба =%lf\n", S);
     printf("Об'єм куба =%lf\n", V);
     system("pause");
